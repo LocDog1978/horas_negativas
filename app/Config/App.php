38f8b4dfa1674d68552b1bd51ee66bb9364a1653
Leandro de Oliveira Carlos <137826392+LocDog1978@users.noreply.github.com>
2024-05-05 13:51:28 -0300

    Mudança na baseurl
    Mudança para horas_negativas

--- app/Config/App.php
+++ app/Config/App.php
@@ -16,7 +16,7 @@ class App extends BaseConfig
 	 *
 	 *    http://example.com/
 	 */
-	public string $baseURL = 'http://localhost/framework_base/';
+	public string $baseURL = 'http://localhost/horas_negativas/';
 
 	/**
 	 * Allowed Hostnames in the Site URL other than the hostname in the baseURL.
