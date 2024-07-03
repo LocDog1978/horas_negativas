a9dc06c6b4d64ada2f9c95559e7f827a9e2ec2f1
LocDog1978 <137826392+LocDog1978@users.noreply.github.com>
2024-07-03 17:05:15 -0300

    feat: Continuação das tabelas

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -20,7 +20,7 @@ $routes->group('cad_negativas', function($routes) {
 	$routes->get('/', 'HorasNegativasController');
 	$routes->post('validate', 'HorasNegativasController::horasValidate');
 	$routes->post('getHorasNegativas', 'HorasNegativasController::getHorasNegativas');
-	$routes->post('tabelaHorasNegativas', 'HorasNegativasController::tabelaHorasNegativas');
+	$routes->get('tabelaHorasNegativas', 'HorasNegativasController::tabelaHorasNegativas');
 });
 
 /*** ---------- Miscellaneous ---------- ***/
